0cba38359c3da6f43ac46363d6d801602839104c
Joaquim Melo <160174239+JoaquiMelo@users.noreply.github.com>
2026-09-21 11:13:36 -0300

    feat: integrar provider de tema

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { UserProvider } from './src/providers/user-provider';
 import { ChatProvider } from './src/providers/chat-provider';
 import { ReviewsProvider } from './src/providers/reviews-provider';
 import { ManagementProvider } from './src/providers/management-provider';
+import { ThemeProvider } from './src/providers/theme-provider';
 
 export default function App() {
   return (
